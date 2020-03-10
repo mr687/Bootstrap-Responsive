e5de7d6daee005155b4cd17c959c2e4fb9d8d4f3
daphino <111201911748@mhs.dinus.ac.id>
2020-03-11 05:19:09 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  Highcharts.chart('chart-stat', {
+
+    title: {
+      text: 'Angka Partisipasi Sekolah SD/MI/Sederajat'
+    },    
+
+    subtitle: {
+        text: ''
+    },
+
+    yAxis: {
+        title: {
+            text: 'Jumlah'
+        }
+    },
+
+    xAxis: {
+        title: {
+            text: 'Tahun'
+        },
+
+        categories: [2015,2016,2017,2018,2019]
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {allowPointSelect: true}
+    },
+
+    series: [{
+        name: ' ',
+        data: [107.03,104.06,99.26,925.01,910.38],
+        showInLegend: false
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+  })
+
+})
